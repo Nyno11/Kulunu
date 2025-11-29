6f35f70fa6a647c5e45e23dfdc6ea3537331741d
DevTiqo <lilfreshy33@yahoo.com>
2025-11-29 22:52:18 +0100

    update: login done

---
+++ core/auth.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const errorDiv = document.getElementById("error");
+    errorDiv.classList.add("d-none");
+
+    try {
+        const res = await fetch("http://localhost:8080/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password })
+        });
+
+        const data = await res.json();
+        console.error(data);
+        if (!data.success) {
+            errorDiv.textContent = data.message;
+            errorDiv.classList.remove("d-none");
+        } else {
+            console.log(data.data['full_name'])
+            alert("Login successful! Welcome " + data.data['full_name']);
+            // redirect or save token/session here
+        }
+    } catch (err) {
+        console.error(err);
+        errorDiv.textContent = "Server error";
+        errorDiv.classList.remove("d-none");
+    }
+});
